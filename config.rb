04b59728d984f8298ec8c80b298a01a916ad88ac
Hahnzhu <hahn.chu91@gmail.com>
2016-07-08 17:15:38 +0800

    a

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "./"
+# http_path = "../../"
 css_dir = "dist/css"
 sass_dir = "src/sass"
 images_dir = "dist/img"
@@ -15,7 +15,7 @@ sourcemap = true
 output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = false
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
